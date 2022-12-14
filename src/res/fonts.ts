4442642993b4a4495cf21d8418f0b889d3acbb91
KarimovAbdullo <109870794+KarimovAbdullo@users.noreply.github.com>
2022-12-14 14:43:11 +0500

    Revert "Home screen"

--- src/res/fonts.ts
+++ src/res/fonts.ts
@@ -4,10 +4,4 @@ export default {
     regular: 'ubuntu-regular',
     medium: 'ubuntu-medium',
   },
-  Montserrat: {
-    monserrat: 'Montserrat-Regular',
-    bold: 'Montserrat-Bold',
-    SemiBold: 'Montserrat-SemiBold',
-    medium: 'Montserrat-Medim',
-  },
 }

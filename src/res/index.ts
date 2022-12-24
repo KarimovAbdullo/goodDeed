a5581d7a6dedb9563502e6a1dc2c3b1e920baa01
Umidbek Usmonov <umidbek.usmonov@zoftify.com>
2022-12-24 11:59:55 +0500

    add lang

--- src/res/index.ts
+++ src/res/index.ts
@@ -3,12 +3,14 @@ import * as consts from './consts'
 import fonts from './fonts'
 import * as fontSize from './fontSize'
 import * as icons from './icons'
+import * as lang from './lang'
 import * as routes from './routes'
 import theme from './theme'
 
 const R = {
   colors,
   theme,
+  lang,
   routes,
   fonts,
   icons,
---
+++ src/res/lang.ts
@@ -0,0 +1,7 @@
+export const navigation_tabs = 'navigation.tabs'
+
+export const screen_login = 'screens.login'
+
+export const component_home = 'components.home'
+
+export const utils_util_name = 'utils.util_name'
